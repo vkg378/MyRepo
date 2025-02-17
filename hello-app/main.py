2482312b3fcacbba5d6ac7fe272dc41ac5534885
Ruvu's Mac <ruchigupta@ruvuss-air.mynetworksettings.com>
2025-02-17 14:43:19 -0500

    test app

---
+++ hello-app/main.py
@@ -0,0 +1,14 @@
+#main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+#name index is only a convention
+def index():
+    name = input('Enter your name:')
+    return print('Hello' +name)
+    #eturn 'congratulations, its a webapp'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8080,debug=True)
